3162bce2f2c06ac333a65c49e2a81d12709d1ccb
Nik Zem <utroranneesr@gmail.com>
2017-02-26 22:28:44 +0200

    Starter file

---
+++ numsum.py
@@ -0,0 +1,9 @@
+def findSumReg(fileName):
+    """
+    Returns amount of all numbers in the input file. Ignores letters, non-characters 
+    like punctuation and spaces.
+
+    fileName: name of input file
+    returns: sum of all numbers
+    """
+    ### TODO
